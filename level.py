b044d24ed936485d6d62155ac4bd8fbef64d8e00
ShashwatS340 <shubhtiwari60737@gmail.com>
2026-04-17 15:56:55 +0530

    level up

---
+++ level.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+# create a backend server for my main application 
+#fake database - a list of dictionaries , each dictionary represent one product
+
+
+products = [
+    {"id": 1, "name": "Shirt", "price": 500, "category": "fashion"},
+    {"id": 2, "name": "Laptop", "price": 50000, "category": "electronics"},
+    {"id": 3, "name": "Shoes", "price": 2000, "category": "fashion"},
+    {"id": 4, "name": "Headphones", "price": 3000, "category": "electronics"},
+
+]
+
+@app.get("/products")  # @app.get is a decorator that creates an api endpoint ; /products - URL route ; category: str - query parameter
+def get_products(category: Optional[str] = None):
+    if category:
+        return [p for p in products if p["category"] == category]
+    return products
+# for p in products → loop through all products
+# if p["category"] == category → check condition
+# [ ... ] → create a new list
+
+@app.get("/products/filter")
+def filter_products(min_price: int, max_price: int):
+    filtered = [p for p in products if min_price <= p["price"] <= max_price]
+    return filtered
+#returns products between given price range 
+
+#optional filters - value can exist OR be none 
+@app.get("/products/search")
+def search_products(
+    category: Optional[str] = None,
+    min_price: Optional[int] = None,
+    max_price: Optional[int] = None
+):
+    result = products
+    if category is not None:
+        result = [p for p in result if p["category"] == category]
+
+    if min_price is not None and max_price is not None:
+        result = [p for p in result if min_price <= p["price"] <= max_price]
+
+    
+
+    return result
+# search products with optional filters - category and price range
+
+    
+
